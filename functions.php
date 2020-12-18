21833de17ab173a27af9f79b4d18e07c5593b100
Ivo Ivanov <pifonsi@gmail.com>
2020-12-18 09:33:31 +0200

    hide woocommerce

--- functions.php
+++ functions.php
@@ -170,3 +170,44 @@ if (class_exists('ACF')) {
 if (class_exists('woocommerce')) {
 	require get_template_directory() . '/inc/woo.php';
 }
+
+/**
+* is_realy_woocommerce_page - Returns true if on a page which uses WooCommerce templates (cart and checkout are standard pages with shortcodes and which are also included)
+*
+* @access public
+* @return bool
+*/
+function is_realy_woocommerce_page () {
+    if( function_exists ( "is_woocommerce" ) && is_woocommerce()){
+        return true;
+    }
+    $woocommerce_keys = array ( "woocommerce_shop_page_id" ,
+        "woocommerce_terms_page_id" ,
+        "woocommerce_cart_page_id" ,
+        "woocommerce_checkout_page_id" ,
+        "woocommerce_pay_page_id" ,
+        "woocommerce_thanks_page_id" ,
+        "woocommerce_myaccount_page_id" ,
+        "woocommerce_edit_address_page_id" ,
+        "woocommerce_view_order_page_id" ,
+        "woocommerce_change_password_page_id" ,
+        "woocommerce_logout_page_id" ,
+        "woocommerce_lost_password_page_id" ) ;
+
+    foreach ( $woocommerce_keys as $wc_page_id ) {
+        if ( get_the_ID () == get_option ( $wc_page_id , 0 ) ) {
+            return true ;
+        }
+    }
+    return false;
+}
+
+add_action( 'template_redirect', 'hide_woocommerce' );
+
+function hide_woocommerce() {
+	$is_admin = current_user_can('manage_options');
+	if ( is_realy_woocommerce_page() && !$is_admin ) {
+		wp_redirect( home_url() );
+		exit();
+	}
+}
