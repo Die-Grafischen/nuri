2a222f76f70a205c53ab6ce4a45fb02f5f897eb1
Ivo Ivanov <pifonsi@gmail.com>
2021-01-28 10:09:29 +0200

    Filter upgrade
    -  hide empty categories from filter
    - style radio buttons

--- inc/woo.php
+++ inc/woo.php
@@ -147,18 +147,20 @@ function woo_categories_filter() {
 				$child_product_cats = get_terms( $child_args );
 
 				foreach ($child_product_cats as $child_product_cat) {
-					echo '<li>
-						<div class="pretty p-default p-fill p-svg p-tada">
-							<input type="checkbox" data-filter=".product_cat-'. esc_attr($child_product_cat->term_id) .'" />
-							<div class="state">
-								<!-- svg path -->
-								<svg class="svg svg-icon" viewBox="0 0 20 20">
-								<path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
-								</svg>
-								<label>'.$child_product_cat->name.'</label>
+					if(!$child_product_cat->count) {
+						echo '<li>
+							<div class="pretty p-default p-fill p-svg p-tada">
+								<input type="checkbox" data-filter=".product_cat-'. esc_attr($child_product_cat->term_id) .'" />
+								<div class="state">
+									<!-- svg path -->
+									<svg class="svg svg-icon" viewBox="0 0 20 20">
+									<path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
+									</svg>
+									<label>'.$child_product_cat->name.'</label>
+								</div>
 							</div>
-						</div>
-					</li>';
+						</li>';
+					}
 				}
 
 			echo '</ul>
