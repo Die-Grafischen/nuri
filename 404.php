a4502f116189106c7f7c48aaebc423722f8c6a3d
Ivo Ivanov <pifonsi@gmail.com>
2021-03-09 21:19:49 +0200

    404

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php get_header();?>
+
+<div class="wrapper">
+
+	<article class="page type-page">
+
+		<blockquote class="wp-block-quote has-text-align-center is-style-large">
+			<p>404 - Seite nicht gefunden.</p>
+		</blockquote>
+
+	</article><!-- .post -->
+
+
+</div>
+
+<?php get_footer(); ?>
